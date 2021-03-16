025d9bb35b48282b98e9ca8cde3472e0f2a2b937
90felipe09 <felhipeshiraishi@gmail.com>
2021-03-16 19:49:33 -0300

    Add: It Works! created channel and genesis block

--- docker/complex-network-local/scripts/create-genesis-block.sh
+++ docker/complex-network-local/scripts/create-genesis-block.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 CLIENT_DIR=./client/org0
-configtxgen -profile OrgsOrdererGenesis -outputBlock $CLIENT_DIR/orderer/genesis.block -channelID syschannel
-configtxgen -profile OrgsChannel -outputCreateChannelTx $CLIENT_DIR/orderer/channel.tx -channelID mychannel
+configtxgen -profile TwoOrgsOrdererGenesis -outputBlock $CLIENT_DIR/orderer/genesis.block -channelID syschannel
+configtxgen -profile TwoOrgsChannel -outputCreateChannelTx $CLIENT_DIR/orderer/channel.tx -channelID mychannel
--- docker/simple-network-local/scripts/create-genesis-block.sh
+++ docker/simple-network-local/scripts/create-genesis-block.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 CLIENT_DIR=./client/org0
-configtxgen -profile OrgsOrdererGenesis -outputBlock $CLIENT_DIR/orderer/genesis.block -channelID syschannel
-configtxgen -profile OrgsChannel -outputCreateChannelTx $CLIENT_DIR/orderer/channel.tx -channelID mychannel
+configtxgen -profile OneOrgOrdererGenesis -outputBlock $CLIENT_DIR/orderer/genesis.block -channelID syschannel
+configtxgen -profile OneOrgChannel -outputCreateChannelTx $CLIENT_DIR/orderer/channel.tx -channelID mychannel
